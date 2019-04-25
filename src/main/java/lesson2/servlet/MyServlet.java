aabdc93e22149080b3888b82a7d403395416c5c6
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-04-25 21:17:06 +0300

    servlet. lesson2

--- src/main/java/lesson2/servlet/MyServlet.java
+++ src/main/java/lesson2/servlet/MyServlet.java
@@ -5,12 +5,12 @@ import lesson2.entity.Item;
 import lesson2.service.ItemService;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 
 @WebServlet(urlPatterns = "/test")
 public class MyServlet extends HttpServlet {
@@ -26,30 +26,39 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream stream = req.getInputStream();
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             Item item = new Item();
-            item = objectMapper.readValue(req.getInputStream(), item.getClass());
-            item.setDateCreated(new Date());
-            item.setLastUpdatedDate(new Date());
+            item = objectMapper.readValue(stream, item.getClass());
             resp.getWriter().println(ItemService.save(item));
         } catch (Exception e) {
             resp.getWriter().println(e.getMessage());
+        } finally {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                System.err.println("Closing streams failed");
+            }
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream stream = req.getInputStream();
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             Item item = new Item();
             item = objectMapper.readValue(req.getInputStream(), item.getClass());
-            Item itemForUpdate = ItemService.findById(item.getId());
-            item.setDateCreated(itemForUpdate.getDateCreated());
-            item.setLastUpdatedDate(new Date());
             resp.getWriter().println(ItemService.update(item));
         } catch (Exception e) {
             resp.getWriter().println(e.getMessage());
+        } finally {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                System.err.println("Closing streams failed");
+            }
         }
     }
 
